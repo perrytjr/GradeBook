8892422d4c4865e8be405e47761fd3ac21bc77ca
Perry Theobald <perrytjr@gmail.com>
2020-12-30 14:04:34 -0800

    first c# console Application

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IBook book = new DiskBook("Perrys Grade Book!");
+            
+
+            book.GradeAdded += OnGradeAdded;
+            EnterGrades(book);
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"For the book name {book.Name}");
+            Console.WriteLine($"The average grade is {stats.Average}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The lowest  grade is {stats.Low}");
+            Console.WriteLine($"The Letter grade is {stats.Letter}");
+        }
+
+        private static void EnterGrades(IBook book)
+        {
+            
+            while (true)
+
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit");
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    break;
+                }
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+
+            }
+        }
+
+        static void OnGradeAdded(object sender, EventArgs e)
+        {
+            Console.WriteLine("a grade was added");
+        }
+
+    }
+}
